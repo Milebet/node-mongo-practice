fdb0f0bf98a338cdb6060fb10862c3999a4bb350
Mileidy Tacuri <mtacuri@devsu.com>
2022-02-03 11:17:24 -0500

    saving old examples

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
-const rect = require('./rectangle');
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
+const promotionsRouter = require('./routes/promotionsRouter');
+const leadersRouter = require('./routes/leadersRouter');
 
-const solveRect = (l, b) => {
-  console.log(`Solving for rectangle with l = ${l} + b = ${b}`);
-  rect(l, b, (err, rectangle) => {
-    if (err) {
-      console.log('ERROR: ', err.message);
-    } else {
-      console.log(`The area of the rectangle of dimensions l = ${l} and b = ${b} is ${rectangle.area()}`);
-      console.log(`The perimeter of the rectangle of dimensions l = ${l} and b = ${b} is ${rectangle.perimeter()}`);
-    }
-  });
-  console.log('This statement is after the call to rectangle');
-};
+const hostname = 'localhost';
+const port = 3000;
 
-solveRect(2, 4);
-solveRect(3, 5);
-solveRect(0, 5);
-solveRect(-3, 5);
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/dishes', dishRouter);
+app.use('/promotions', promotionsRouter);
+app.use('/leaders', leadersRouter);
+
+app.use(express.static(`${__dirname}/` + '../public'));
+
+app.use((req, res, next) => {
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+
+});
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+})
